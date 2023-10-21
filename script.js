05160e1f5e973927cadccf0ba8a418ec307eff4a
ravkamaria <131477170+ravkamaria@users.noreply.github.com>
2023-10-21 13:21:12 +0300

    done tasks from chapter 8

--- script.js
+++ script.js
@@ -96,6 +96,93 @@
 //Chapter 7, page 153///////////////////////////////////////////////////////////////////////////////////////////////////////
 //Гра "Шибениця"
 
+// const words = [
+//   "javascript",
+//   "monkey",
+//   "amazing",
+//   "pancake",
+//   "elephant",
+//   "icecream",
+// ];
+
+// let word = words[Math.floor(Math.random() * words.length)];
+// console.log(word);
+
+// const answerArray = [];
+// for (let i = 0; i < word.length; i++) {
+//   answerArray[i] = "_";
+//   // answerArray.push("_");
+// }
+// console.log(answerArray);
+// let remainingLetters = word.length;
+// let remainingAttempts = 15;
+
+// while (remainingLetters > 0 && remainingAttempts > 0) {
+//   alert("Guess the word " + answerArray.join(" "));
+//   switch (remainingAttempts) {
+//     case 0:
+//       alert("Game over!");
+//       break;
+//     case 1:
+//       alert(remainingAttempts + " attempt left");
+//       break;
+//     default:
+//       alert(remainingAttempts + " attempts left");
+//   }
+//   let guess = prompt("Guess a letter, or click Cancel to stop playing.");
+//   if (guess === null) {
+//     alert("Game over!");
+//     break;
+//   } else if (guess.length !== 1) {
+//     alert("Please enter a single letter.");
+//   } else if (answerArray.includes(guess.toLowerCase())) {
+//     alert("Letter " + guess.toLowerCase() + " is already opened!");
+//     remainingAttempts++;
+//   } else {
+//     for (let j = 0; j < word.length; j++) {
+//       if (word[j] === guess.toLowerCase()) {
+//         answerArray[j] = guess.toLowerCase();
+//         remainingLetters--;
+//       }
+//     }
+//   }
+//   remainingAttempts--;
+// }
+// alert(answerArray.join(" "));
+// alert("Good job! The answer was " + word);
+
+//Chapter 8, page 173///////////////////////////////////////////////////////////////////////////////////////////////////////
+//#1 Рахуємо з функціями 36325 * 9824 + 777.
+
+// function add(a, b){
+//   return a + b;
+// };
+
+// function multiply(a, b){
+//   return a * b;
+// };
+
+// console.log(add(multiply(36325, 9824), 777));
+
+//#2 Напишіть функцію, яка перевіряє чи два масиви є однакові.
+
+// function areArraysSame(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     if(arr1[i] !== arr2[i]){
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+//  console.log(areArraysSame([1, 2, 3], [4, 5, 6]));
+//  console.log(areArraysSame([1, 2, 3], [1, 2, 3]));
+//  console.log(areArraysSame([1, 2, 3], [1, 2, 3, 4]));
+
+//#3 Створюємо гру Шибениця, використовуючи функції.
+
 const words = [
   "javascript",
   "monkey",
@@ -105,20 +192,26 @@ const words = [
   "icecream",
 ];
 
-let word = words[Math.floor(Math.random() * words.length)];
-console.log(word);
+let word = pickWord(words);
 
-const answerArray = [];
-for (let i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
-  // answerArray.push("_");
+function pickWord(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]; //повертаємо випадкове число;
 }
-console.log(answerArray);
+
+const answerArray = setupAnswerArray(word);
+function setupAnswerArray(word) {
+  const ansArr = [];
+  for (let i = 0; i < word.length; i++) {
+    ansArr[i] = "_";
+  }
+  return ansArr; //повертаємо масив відповідей;
+}
+
 let remainingLetters = word.length;
 let remainingAttempts = 15;
 
 while (remainingLetters > 0 && remainingAttempts > 0) {
-  alert("Guess the word " + answerArray.join(" "));
+  showPlayerProgress(answerArray);
   switch (remainingAttempts) {
     case 0:
       alert("Game over!");
@@ -129,7 +222,8 @@ while (remainingLetters > 0 && remainingAttempts > 0) {
     default:
       alert(remainingAttempts + " attempts left");
   }
-  let guess = prompt("Guess a letter, or click Cancel to stop playing.");
+  let guess = getGess();
+
   if (guess === null) {
     alert("Game over!");
     break;
@@ -139,14 +233,34 @@ while (remainingLetters > 0 && remainingAttempts > 0) {
     alert("Letter " + guess.toLowerCase() + " is already opened!");
     remainingAttempts++;
   } else {
-    for (let j = 0; j < word.length; j++) {
-      if (word[j] === guess.toLowerCase()) {
-        answerArray[j] = guess.toLowerCase();
-        remainingLetters--;
-      }
-    }
+    let correctGuesses = updateGameState(guess, word, answerArray);
+    remainingLetters -= correctGuesses;
   }
   remainingAttempts--;
 }
-alert(answerArray.join(" "));
-alert("Good job! The answer was " + word);
+showAnswerAndCongratulatePlayer(answerArray, word);
+
+function showPlayerProgress(answerArray) {
+  alert("Guess the word " + answerArray.join(" ")); //Використовуємо alert, щоб показати гравцю його прогрес;
+}
+
+function getGess() {
+  return prompt("Guess a letter, or click Cancel to stop playing."); //Використовуємо запит на введення, щоб отримати варіант відповіді;
+}
+
+function updateGameState(guess, word, answerArray) {
+  let guessedLetter = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === guess.toLowerCase()) {
+      answerArray[i] = guess.toLowerCase(); //Оновлюємо масив відповідей та повертаємо число, що показує ск разів даний варіант
+      guessedLetter += 1; //відповіді фігурує у слові, щоб масив remainingLetters міг оновитися;
+    }
+  }
+  return guessedLetter;
+}
+
+function showAnswerAndCongratulatePlayer(answerArray, word) {
+  alert(answerArray.join(" ")); //Використовуємо alert, щоб показати відповідь та привітати гравця.
+  alert("Good job! The answer was " + word);
+}
+
